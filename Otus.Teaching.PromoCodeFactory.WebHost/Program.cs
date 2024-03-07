8daf14ac5fb4391c17033e676e1eafdde77cf96e
Artem Artemyev <columbo.art@gmail.com>
2024-03-07 15:24:12 +0300

    Project skeleton

---
+++ Otus.Teaching.PromoCodeFactory.WebHost/Program.cs
@@ -0,0 +1,31 @@
+using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
+using Otus.Teaching.PromoCodeFactory.Core.Domain.Administration;
+using Otus.Teaching.PromoCodeFactory.DataAccess.Data;
+using Otus.Teaching.PromoCodeFactory.DataAccess.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddScoped(typeof(IRepository<Employee>), (x) => 
+    new InMemoryRepository<Employee>(FakeDataFactory.Employees));
+builder.Services.AddScoped(typeof(IRepository<Role>), (x) => 
+    new InMemoryRepository<Role>(FakeDataFactory.Roles));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.Run();
