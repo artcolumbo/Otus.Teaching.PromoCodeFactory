89da33fd88544bb0deea111cd54be7850fcaf173
Artem Artemyev <columbo.art@gmail.com>
2024-03-22 12:27:47 +0300

    [CF] CRUD операции в EmployeesController

---
+++ Otus.Teaching.PromoCodeFactory.WebHost/Models/Employee/UpdateEmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Otus.Teaching.PromoCodeFactory.WebHost.Models.Employee;
+
+public class UpdateEmployeeModel
+{
+    public Guid Id { get; set; }
+    
+    public string? FirstName { get; set; }
+        
+    public string? LastName { get; set; }
+
+    public string? Email { get; set; }
+    
+    public int AppliedPromoCodesCount { get; set; }
+
+    public List<Core.Domain.Administration.Role>? Roles { get; set; }
+}
